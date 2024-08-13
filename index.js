c59e6f30c5f68787f6bf0fffec3caee570b07836
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-13 19:36:18 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send(`Car Doctor server is running at: ${port}`)
+})
+
+app.listen(port, ()=> {
+    console.log("Server is running at: ", port);
+})
