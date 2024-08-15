3de4ed931615d750507d98c2df22b551ad1aedc6
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-15 23:37:50 +0600

    verify access token

--- index.js
+++ index.js
@@ -25,6 +25,23 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(!authorization){
+        res.status(401).send({error: true, message: "Unauthorized Aceess"});
+    }
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded)=> {
+        if(error){
+            return res.status(403).send({error: true, message: "Unauthorized Access"});
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -57,8 +74,13 @@ async function run() {
         })
 
         //get all checkout data
-        app.get('/checkouts', async (req, res) => {
-            console.log(req.query);
+        app.get('/checkouts', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            console.log("comeback:", decoded);
+            if(decoded.email !== req.query.email){
+                return res.status(403).send({error: true, message: "Forbidden Access"});
+            }
+            // console.log(req.headers.authorization);
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email };
